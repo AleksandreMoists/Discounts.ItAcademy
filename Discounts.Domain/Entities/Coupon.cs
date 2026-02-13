be3b4c59cc26abb1095daedcb04a56f42fb83c67
AleksandreMoists <aleqsandre.moistsrapishvili.1@btu.edu.ge>
2026-02-14 03:00:17 +0400

    Feat: refactor Coupon entity

--- Discounts.Domain/Entities/Coupon.cs
+++ Discounts.Domain/Entities/Coupon.cs
@@ -1,7 +1,12 @@
-﻿namespace Discounts.Domain.Entities;
+﻿using Discounts.Domain.Enums;
+
+namespace Discounts.Domain.Entities;
 
 public class Coupon : BaseEntity
 {
     public string Code { get; set; } = string.Empty;
     public decimal Discount { get; set; }
+    public CouponStatus Status { get; set; }
+    
+    public Offer Offer { get; set; } // Navigation property
 }
